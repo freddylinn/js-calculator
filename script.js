c149250cacbc3ce2b73c76c5e816c1a1e5ebf537
Freddy <frederick.linn@colorado.edu>
2022-06-19 16:02:29 -0600

    adding equals functionality - no edge case coverage yet

--- script.js
+++ script.js
@@ -18,40 +18,112 @@ function power(num1,num2){
     return num1 ** num2;
 }
 
+function mod(num1,num2){
+    return num1 % num2;
+}
+
 function operate(operatorName,num1,num2){
     switch(operatorName){
 
-        case "add":
-            add(num1,num2);
+        case "+":
+            return add(num1,num2);
         break;
 
-        case "subtract":
-            subtract(num1,num2);
+        case "-":
+            return subtract(num1,num2);
         break;
 
-        case "multiply":
-            multiply(num1,num2);
+        case "*":
+            return multiply(num1,num2);
         break;
 
-        case "divide":
-            divide(num1,num2);
+        case "/":
+            return divide(num1,num2);
+        break;
+
+        case "x^":
+            return power(num1,num2);
+        break;
+
+        case "%":
+            return mod(num1,num2);
         break;
 
     }
 }
 
+function calculateAns(){
+    if(displayElement.textContent.includes(".")){
+        secondNumber = parseFloat(displayElement.textContent);
+    }
+    else{
+        secondNumber = parseInt(displayElement.textContent);
+    }
+    displayElement.textContent = operate(currOperator,firstNumber,secondNumber);
+    secondStage = false;
+}
+
 
 const displayElement = document.querySelector(".currNumber");
 const numberButtons = document.querySelectorAll(".number");
+let reset = true;
+let secondStage = false;
+let firstNumber = "";
+let secondNumber = "";
+let currOperator = "";
 
 for(const button of numberButtons){
     button.addEventListener("click", function(e){
-        if(displayElement.textContent.length < 12)
-        displayElement.textContent += e.target.textContent;
+        if(displayElement.textContent.length < 12 && !reset){
+            displayElement.textContent += e.target.textContent;
+        }
+        else{
+            displayElement.textContent = e.target.textContent;
+            reset = false;
+        }
     });
 }
 
 
 const clearButton = document.querySelector("#clear");
-clearButton.addEventListener("click", () => displayElement.textContent = "");
+clearButton.addEventListener("click", () => { displayElement.textContent = "0"; reset = true; });
 
+
+const operatorButtons = document.querySelectorAll(".operator");
+
+for(const button of operatorButtons){
+    button.addEventListener("click", (e) => {
+        if(!secondStage){
+            if(displayElement.textContent.length >= 1){
+
+                currOperator = e.target.textContent;
+
+                if(displayElement.textContent.includes(".")){
+                    firstNumber = parseFloat(displayElement.textContent);
+                }
+                else{
+                    firstNumber = parseInt(displayElement.textContent);
+                }
+
+                reset = true;
+                secondStage = true;
+
+            }
+        }
+        else{
+            calculateAns();
+        }
+    });
+}
+
+
+const decimalButton = document.querySelector("#decimal");
+decimalButton.addEventListener("click", () => {
+    if(!displayElement.textContent.includes(".")){
+        reset = false;
+        displayElement.textContent += ".";
+    }
+})
+
+const equalsButton = document.querySelector("#equals");
+equalsButton.addEventListener("click", calculateAns);
